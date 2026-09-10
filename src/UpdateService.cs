b44a30da87da0a8ec73ce778e4921a99d4c3ba2c
Tomasz Gołaszewski <tomaasz@gmail.com>
2026-09-10 14:40:16 +0200

    Bump version to 1.1.0 for native C# release

--- src/UpdateService.cs
+++ src/UpdateService.cs
@@ -28,7 +28,7 @@ namespace ScrcpyManager
 
     public class UpdateService
     {
-        public const string CurrentVersion = "1.0.0";
+        public const string CurrentVersion = "1.1.0";
         public GitHubRelease LatestRelease { get; private set; }
 
         public async Task<bool> CheckForUpdateAsync()
